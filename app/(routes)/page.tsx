114a42949af155448b1f2328646c583e154dd20f
AsrafRD <146421331+AsrafRD@users.noreply.github.com>
2023-11-02 09:52:39 +0700

    Modif Ui Store (#1)

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,7 @@
 import getBillboard from "@/actions/get-billboard";
+import getCategories from "@/actions/get-categories";
 import getProducts from "@/actions/get-products";
+import MainNav from "@/components/main-nav";
 import ProductList from "@/components/product-list";
 import Billboard from "@/components/ui/billboard";
 import Container from "@/components/ui/container";
@@ -8,15 +10,19 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("");
-  // const billboard = await getBillboard("1836815f-c264-4a91-b5a5-190557c1cb09");
+  const billboard = await getBillboard("1836815f-c264-4a91-b5a5-190557c1cb09");
+  const categories = await getCategories();
 
   return (
     <Container>
-      <div className="space-y-10 pb-10">
+      <div className="space-y-5 pb-10">
         <Billboard 
           data={billboard}
         />
+        <div className="border">
+          
+        <MainNav data={categories} />
+        </div>
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
         </div>
